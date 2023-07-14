7b397bdb7bde577ca590de35d8c96a2efcda59b2
Chika Okafor <ochika6363@gmail.com>
2023-07-13 19:20:40 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ int main(void)
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
-	return (1);
+	return (0);
 }
 
