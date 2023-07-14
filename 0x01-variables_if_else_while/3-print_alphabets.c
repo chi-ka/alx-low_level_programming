1a8af7995f58c79db2878670533e944cff30bb1a
Chika Okafor <ochika6363@gmail.com>
2023-07-14 15:20:41 -0700

    Write alphabets in reverse

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@ int main(void)
 	char a;
 	char Lowercase[] = {'a','b','c','d','e','f','g','h','i','j',\
 		'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-	char Uppercase[] = {'A','B','C','D','E','D','F','G','H','I','J','K','L','M','N','O','P',\
+	char Uppercase[] = {'A','B','C','D','E', 'F', 'G', 'H','I','J','K','L','M','N','O','P',\
 		'Q','R','S','T','U','V','W','X','Y','Z'};
 	
 	for(i = 0; i < (int)sizeof(Lowercase)/sizeof(Lowercase[0]); i++)
