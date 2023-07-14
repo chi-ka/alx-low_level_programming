dbb6b2378c5f1f8288a3eb4c0e4dfd11044fcc83
Chika Okafor <ochika6363@gmail.com>
2023-07-13 19:48:39 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 int main(void)
 
 {
-	const char\* out = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char* out = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fwrite(out, sizeof(char), strlen(out), stderr);
 
