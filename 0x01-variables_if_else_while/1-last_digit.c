ae5bc4ed1dd1e456955f4f33207422b9662930c5
Chika Okafor <ochika6363@gmail.com>
2023-07-14 13:40:26 -0700

    Print lower case alphabets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,1 +1,32 @@
-echo /bin/bash
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Main- Entry point */
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/* Return: Always 0 (Success);*/
+
+int main(void)
+{
+	int n;
+	int lastdigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastdigit = n % 10;
+	if(n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",n,lastdigit);
+	}
+	else if(n == 0)
+        {
+                printf("Last digit of %d is %d and is 0\n",n,lastdigit);
+        }
+	else
+        {
+                printf("Last digit of %d is %d and is less than 6 and not 0\n",n,lastdigit);
+        }
+
+	return (0);
+}
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,24 @@
-echo /bin/bash
+#include <stdio.h>
+
+/* Main- Entry point */
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/* Return: Always 0 (Success);*/
+
+int main(void)
+{
+	size_t i;
+	char a;
+	char alphabets[] = {'a','b','c','d','e','f','g','h','i','j',\
+		'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	
+	for(i = 0; i < (int)sizeof(alphabets)/sizeof(alphabets[0]); i++)
+	{
+		a = alphabets[i];
+		putchar(a);
+		putchar('\n');
+	}
+
+	return(0);
+
+}
