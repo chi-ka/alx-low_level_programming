118326bfd43a680a7861b3f5f20d58701300eaef
Chika Okafor <ochika6363@gmail.com>
2023-07-18 11:09:25 -0700

    Print small letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_alphabet(void)
+{
+        int i;
+
+        for (i = 'a'; i <= 'z'; ++i)
+        {
+                putchar(i);
+        }
+        putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+int print_alphabet(void);
+
+#endif /* MYHEADER_H */
