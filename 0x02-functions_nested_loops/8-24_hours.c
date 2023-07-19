37c4dfcf57d75bcc3c8d7f2917af4dd6671ec4f5
Chika Okafor <ochika6363@gmail.com>
2023-07-19 04:20:35 -0700

    Count time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 
-/** Print every minute of the life of Jack
+/**
+ * jack_bauer - miute of the life of Jack
+ *
  * Prototype: void jack_bauer(void);
+ *
+ * Return: nothing
+ *
  * You can listen to this soundtrack while coding :)
  */
 
 void jack_bauer(void)
 {
 	int hour, minute;
-	for (hour = 0; hour < 24; hour++) 
+
+	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
