b3d83f5a260fef344bbe31f3a7cc7365f0eec1c5
Chika Okafor <ochika6363@gmail.com>
2023-07-20 17:24:25 -0700

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,15 @@
 #include "main.h"
 
 /**
- * main - Entry point 
- * 
+ * print_numbers - Entry point
+ *
  * print_number - Prints numbers from 0-9
  */
 
 void print_numbers(void)
 {
 	int i;
-        
+
 	for (i = '0'; i <= '9'; ++i)
 	{
 		_putchar(i);
