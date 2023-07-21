63dd207195e0fc2cb855571b1ceddbc580840526
Chika Okafor <ochika6363@gmail.com>
2023-07-20 17:21:13 -0700

    done

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,20 +2,20 @@
 #include "main.h"
 
 /**
- * main - multiply two integers.
+ * mul - multiply two integers.
  *
  * @a: one of the integers to multiply
  *
  * @b: one of the integers to multiply
- * 
+ *
  * Return: 1 if uppercase and 0 otherwise.
  */
 
 int mul(int a, int b)
 {
-        int c;
-        
-        c = a * b;
-        
-        return (c);
+	int c;
+
+	c = a * b;
+
+	return (c);
 }
