6dea33b67f7cff38f1baf2f1e99b89215ffaa52b
Chika Okafor <ochika6363@gmail.com>
2023-07-21 05:09:00 -0700

    print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,23 +2,19 @@
 #include "main.h"
 
 /**
- * print_number - prints a number
- *
- * Return: Always 0 (Success)
+ * print_numbers - Print the numbers since 0 up to 9
+ * Return: The numbers since 0 up to 9
  */
 
-void print_number(int n)
+void print_numbers(void)
+
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
 
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
+	int x;
 
-	_putchar(n % 10 + '0');
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
 }
