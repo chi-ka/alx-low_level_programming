46e6c9a606416fb9f99a88b35e4a5eb0ff40182f
Chika Okafor <ochika6363@gmail.com>
2023-07-25 17:44:22 -0700

    done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,38 +1,75 @@
 #include <stdio.h>
+#include "main.h"
+#include <math.h>
 
-int _atoi(char *s) {
-    int result = 0;
-    int sign = 1;
+/**
+ * _pow - converts string to int
+ *
+ * @a: int input
+ *
+ * @b: power input
+ *
+ * Return: int output
+ */
 
-    // Skip leading whitespace characters
-    while (*s == ' ' || *s == '\t' || *s == '\n' || *s == '\r' || *s == '\f' || *s == '\v')
-        s++;
-
-    // Handle optional sign
-    while (*s == '-' || *s == '+') {
-        if (*s == '-')
-            sign *= -1;
-
-        s++;
-    }
-
-    // Convert numeric characters to an integer
-    while (*s >= '0' && *s <= '9') {
-        int digit = *s - '0';
-
-        // Check for integer overflow
-        if (result > (INT_MAX - digit) / 10) {
-            if (sign == 1) {
-                return INT_MAX;
-            } else {
-                return INT_MIN;
-            }
-        }
-
-        result = result * 10 + digit;
-        s++;
-    }
-
-    return result * sign;
+int _pow(double a, double b)
+{
+	int i;
+	
+	i = a;
+	while (b > 1)
+	{
+		a *= i;
+		b--;
+	}
+	return (a);
 }
 
+/**
+ * _atoi - converts string to int
+ *
+ * @s: input char
+ *
+ * REturn: an int 
+ */
+
+int _atoi(char *s)
+{
+	char *a;
+	char *z;
+	char c = '\0';
+	int place;
+	int count = 0;
+	int number = 0;
+	
+	while (*s != '\0')
+	{
+		if ('0' <= *s && *s <= '9')
+        {
+		count ++;
+		if (count == 1)
+		{
+			a = s;
+			z = s;
+			if (*(s - 1) == '-')
+			{
+				c = '-';
+			}
+		}
+		z++;
+	}
+	s++;
+	}
+	while (a < z)
+	{
+		place = _pow(10,(count-1));
+		number += ((*a - '0') * place);
+		a++;
+		count--;
+	}
+	if (c == '-')
+	{
+		number = -number;
+	}
+	return (number);
+}
