a91278793427cc3bc6915896911e3e016314ec6a
Chika Okafor <ochika6363@gmail.com>
2023-07-27 16:35:05 -0700

    print numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ *
+ * @n: input number
+ */
+
+void print_number(int n)
+{
+	int d = 1;
+	int tmp = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		tmp = n = -n;
+	}
+	while (tmp > 9)
+	{
+		tmp /= 10;
+		d *= 10;
+	}
+	while ( d != 0)
+	{
+		_putchar(n / d + '0');
+		n %= d;
+		d /= 10;
+	}
+}
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints number
+ *
+ * @n: input int 
+ */
+
+int _putchar(char c) 
+{
+    return putchar(c);
+}
