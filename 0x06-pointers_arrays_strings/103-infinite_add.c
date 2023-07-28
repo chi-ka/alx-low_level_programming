15724f83b8151c06d3e9382e3b0e1c98e1804f3f
Chika Okafor <ochika6363@gmail.com>
2023-07-27 17:57:08 -0700

    print 98

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-char* infinite_add(char* n1, char* n2, char* r, int size_r)
+/**
+ * infinite_add - adds two integers
+ *
+ * @n1: integer input
+ *
+ * @n2: integer input
+ *
+ * @r: char input
+ *
+ * @size_r: size of r
+ *
+ * Return: returns a char
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int len1 = strlen(n1);
 	int len2 = strlen(n2);
-	int carry = 0;
+	int carry, k = 0;
 	int i = len1 - 1;
 	int j = len2 - 1;
-	int k = 0;
-	int digit1;
-	int digit2;
-	int sum;
-	int len_r;
-	int m;
-	int temp;
+	int digit1, digit2, sum, len_r, m, temp;
 
 	if (size_r <= len1 || size_r <= len2)
 	{
 		return (0);
 	}
-	
 	while (i >= 0 || j >= 0 || carry != 0)
 	{
 		digit1 = (i >= 0) ? n1[i] - '0' : 0;
@@ -28,14 +35,11 @@ char* infinite_add(char* n1, char* n2, char* r, int size_r)
 		sum = digit1 + digit2 + carry;
 		r[k] = sum % 10 + '0';
 		carry = sum / 10;
-		
 		i--;
 		j--;
 		k++;
 	}
-	
 	len_r = k;
-	
 	for (m = 0; m < len_r / 2; m++)
 	{
 		temp = r[m];
