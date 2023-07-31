8c9d84cab52f908218c76476a4ac6eb346964865
Chika Okafor <ochika6363@gmail.com>
2023-07-31 09:10:12 -0700

    strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ void _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 void set_string(char **s, char *to);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
