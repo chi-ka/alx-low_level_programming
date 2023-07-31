3f8b37adf1bd62bc20e621d265332f913a7057ea
Chika Okafor <ochika6363@gmail.com>
2023-07-31 07:23:47 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ *
+ * @dest: destination of copy
+ *
+ * @src: src of copy
+ *
+ * @n amount to copy
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	return (dest);
+}
