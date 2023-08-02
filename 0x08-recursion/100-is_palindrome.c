d5681fc6d83b0a4796e52005318005a94d687ba3
Chika Okafor <ochika6363@gmail.com>
2023-08-02 15:09:41 -0700

    done

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * is_palindrome_helper - helps main function
+ *
+ * @s: word to check
+ *
+ * @start: start of the word
+ *
+ * @end: end of the word
+ *
+ * Return: int
+ */
+
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	
+	}
+
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+
+	return (is_palindrome_helper(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrime - Checks if a word is a palandrome
+ *
+ * @s: string to check the word
+ *
+ * Return: returns an int
+ */
+
+int is_palindrome(char *s)
+{
+	int length;
+       
+	length = strlen(s);
+
+	if (length <= 1)
+	{
+		return (1);
+	}
+	
+	return (is_palindrome_helper(s, 0, length - 1));
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * wildcmp_helper - main string
+ *
+ * @s1 - first string
+ *
+ * @s2 - second string
+ *
+ * Return: int
+ */
+
+int wildcmp_helper(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s2 == '*')
+	{
+		while (*(s2 + 1) == '*')
+		{
+			s2++;
+		}	
+		while (*s1 != '\0')
+		{
+			if (wildcmp_helper(s1, s2 + 1))
+			{
+				return (1);
+			}
+			s1++;
+		}		
+		return (wildcmp_helper(s1, s2 + 1));
+	}
+
+	if (*s1 == *s2 || *s2 == '?')
+	{
+		return (wildcmp_helper(s1 + 1, s2 + 1));
+	}
+
+	return (0);
+}
+
+/**
+ * wildcmp - compare strings
+ *
+ * @s1: first string
+ *
+ * @s2: second string
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	return wildcmp_helper(s1, s2);
+}
