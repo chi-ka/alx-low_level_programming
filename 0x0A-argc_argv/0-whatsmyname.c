650cea7556ea4531dbb0afef79d58192422fc778
Chika Okafor <ochika6363@gmail.com>
2023-08-04 16:47:02 -0700

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,19 +3,6 @@
 #include <unistd.h>
 
 /**
- * _putchar - prints a char
- *
- * @c: input char
- *
- * Return: int
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * main - main function
  *
  * @argc: input argc count
--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - prints a char
+ *
+ * @c: input char
+ *
+ * Return: int
+ */
 
 int _putchar(char c)
 {
-    return write(1, &c, 1);
+        return (write(1, &c, 1));
 }
