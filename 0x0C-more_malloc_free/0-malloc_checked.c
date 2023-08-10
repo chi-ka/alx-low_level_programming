9860dcdb5ff12ce185d7bc0e36e346561348b22f
Chika Okafor <ochika6363@gmail.com>
2023-08-10 15:13:37 -0700

    malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,17 +6,17 @@
  * @b: The number of bytes to allocate.
  *
  * Returns: A pointer to the allocated memory.
- * If malloc fails, the function causes normal process termination with a status value of 98.
+ * If malloc fails, terminate with value of 98.
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
-	
+
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
-	
+
 	return (ptr);
 }
