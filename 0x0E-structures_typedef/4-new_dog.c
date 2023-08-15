ac6875bd34a64ec313cb72ae287ff087cef61b37
Chika Okafor <ochika6363@gmail.com>
2023-08-15 15:29:22 -0700

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,27 +3,66 @@
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog instance.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * _str_len - Calculate the length of a string
+ * @str: The input string
  *
- * Return: Pointer to the new dog instance, or NULL if memory allocation fails.
+ * Return: The length of the string
+ */
+
+int _str_len(char *str)
+{
+	int length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	
+	return (length);
+}
+
+/**
+ * new_dog - Creates a new dog with given information
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Return: A pointer to the new dog, or NULL if failed
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *new_dog;
+	int i;
+	int name_len = _str_len(name);
+	int owner_len = _str_len(owner);
+	dog_t *new_dog = malloc(sizeof(dog_t));
 
-	new_dog = malloc(sizeof(struct dog));
-
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
-	new_dog->name = name;
+	new_dog->name = malloc(sizeof(char) * (name_len + 1));
+	new_dog->owner = malloc(sizeof(char) * (owner_len + 1));
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; i <= name_len; i++)
+	{
+		new_dog->name[i] = name[i];
+	}
+	for (i = 0; i <= owner_len; i++)
+	{
+		new_dog->owner[i] = owner[i];
+	}
 	new_dog->age = age;
-	new_dog->owner = owner;
-
+	
 	return (new_dog);
 }
+
