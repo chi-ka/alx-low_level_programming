07e7304ea26e427c85e2375f74a010f2a3a94eb0
Chika Okafor <ochika6363@gmail.com>
2023-08-14 21:26:53 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Short description
  *
- * @dog: First member
+ * @name: First member
  *
  * @age: Second member
  *
@@ -25,3 +25,5 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif /* DOG_H */
