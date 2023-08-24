e4f77c721384b397c2039b9b8fb5eba68350d5a0
Chika Okafor <ochika6363@gmail.com>
2023-08-24 10:58:24 -0700

    linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stddef.h> 
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL) 
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n",h->len, h->str);
+		}
+		else 
+		{
+			printf("[0] (nil)\n");
+		}
+
+		h = h->next;
+		count++;
+	}
+
+	return count;
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct list_t
+{
+    char *str;
+    int len;
+    struct list_t *next;
+}list_t;
+
+
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
+
