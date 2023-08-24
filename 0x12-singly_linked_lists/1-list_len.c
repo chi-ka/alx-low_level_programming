2c607ce2f245fb15b8bbed025cfd4baff7f373b2
Chika Okafor <ochika6363@gmail.com>
2023-08-24 11:24:20 -0700

    linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,28 +5,19 @@
 #include "lists.h"
 
 /**
- * print_list - prints a linked list.
+ * list_len - prints a linked list.
  *
  * @h: pointer to the list
  *
  * Return: size of the linking
  */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str != NULL)
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		else
-		{
-			printf("[0] (nil)\n");
-		}
-
 		h = h->next;
 		count++;
 	}
