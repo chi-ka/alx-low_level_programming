f525c7273be74af319c1387a70099f3a732922e5
Chika Okafor <ochika6363@gmail.com>
2023-08-25 13:25:54 -0700

    linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,7 +10,7 @@ void pre_main(void) __attribute__((constructor));
 
 void pre_main(void)
 {
-	char *line1 = "You're beat! and yet, you must allow,\nI bore my house upon my back\n";
+	char *line1 = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
 
 	int i = 0;
 	while (line1[i] != '\0')
