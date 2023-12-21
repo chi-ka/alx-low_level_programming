bc4331d41c07b6b281e3cd649a8af920d4c2dcce
Chika Okafor <ochika6363@gmail.com>
2023-12-20 17:14:22 -0800

    done

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    '''
+    Gets the perimiter of an island
+    '''
+    build_island = 0
+    for island in grid:
+        for landzone in island:
+            if landzone == 1:
+                build_island += 1
+    
+    if build_island == 1:
+        return 4
+    elif build_island == 2:
+        return 6
+    elif build_island > 2:
+        return (6 + ( (build_island - 2) * 2))
