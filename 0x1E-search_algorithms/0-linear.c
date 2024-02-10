276f4b21868347b5b382d5c6789df996710e6c38
chika okafor <ochika6363@gmail.com>
2024-02-10 05:09:22 -0800

    done

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
+/**
+ * linear_search - Entry point
+ *
+ * *array - array to search
+ * size - size of the array
+ * value -  value to search for
+ * Return: number or -1 if array is null or not seen
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
+
 	if (array == NULL)
 		return -1;
 
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return i;
 	}
 
-	return;
+	return -1;
 }
